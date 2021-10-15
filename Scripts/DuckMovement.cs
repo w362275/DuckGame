9a7585ad558919ec8e033e4323bcadcb44294e21
Alex <44437464+w362275@users.noreply.github.com>
2021-10-15 11:07:43 +0100

    Respawn changes
    Made it so that player can respawn and have a short period without being attacked by existing ducks.

--- Scripts/DuckMovement.cs
+++ Scripts/DuckMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DuckMovement : MonoBehaviour
 {
@@ -9,11 +10,14 @@ public class DuckMovement : MonoBehaviour
     float attackDelay = 3f;
     float lastAttack = 0f;
     public ShootPlayer attackPlayer;
+    public Slider playerHealth;
+    float delayDuck = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerHealth = GameObject.Find("health_bar").GetComponent<Slider>();
+        Debug.Log(playerHealth.value);
     }
 
     // Update is called once per frame
@@ -22,16 +26,30 @@ public class DuckMovement : MonoBehaviour
         float angleDif = Mathf.Atan2(player.transform.position.x - transform.position.x, player.transform.position.z - transform.position.z) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, angleDif, 0f);        //Rotates duck to face player
 
-        float xSide = player.transform.position.x - transform.position.x;
-        float zSide = player.transform.position.z - transform.position.z;
-        float hypotenuse = Mathf.Sqrt((xSide * xSide) + (zSide * zSide));       //Pythagoras calculates direct distance to player
-
-        if (hypotenuse > 10f)
+        if (delayDuck <= 0f)
         {
-            transform.Translate(0f, 0f, 0.02f);     //Duck moves towards player if hypotenuse length is great enough
+            float xSide = player.transform.position.x - transform.position.x;
+            float zSide = player.transform.position.z - transform.position.z;
+            float hypotenuse = Mathf.Sqrt((xSide * xSide) + (zSide * zSide));       //Pythagoras calculates direct distance to player
+
+            if (hypotenuse > 10f)
+            {
+                transform.Translate(0f, 0f, 0.02f);     //Duck moves towards player if hypotenuse length is great enough
+            }
+
+            ManageAttacks(hypotenuse);
         }
-        
-        ManageAttacks(hypotenuse);
+
+        if (playerHealth.value <= 0f)
+        {
+            PlayerRespawn();        //Calls function to delay the duck if player is dead
+        }
+
+        if (delayDuck > 0f)
+        {
+            delayDuck -= Time.deltaTime;        //Decrements delayDuck until it hits 0
+            Debug.Log(delayDuck);
+        }
     }
 
     void ManageAttacks(float playerDistance)
@@ -55,5 +73,11 @@ public class DuckMovement : MonoBehaviour
         {
             Destroy(gameObject);        //Destroys duck once it hits 0 health
         }
+    }
+    
+    void PlayerRespawn()
+    {
+        delayDuck = 5f;
+        Debug.Log("Delay set to " + delayDuck);
     }
 }
